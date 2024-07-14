afff01cc4d378f7bedd1ae417ea67d89dc87765c
Abinash Sahoo <abinashsahoo063@gmail.com>
2024-07-14 23:04:41 +0530

    COMPLETEJAVASCRIPT

---
+++ first.js
@@ -0,0 +1,92 @@
+// console.log("Hello Abinash!");
+
+// VARIABLE IN JS :
+// name = "Tony stark";
+// console.log(name);
+// age = 50;
+// console.log(age);
+// price = 234.55;
+// console.log(price);
+// x = null;
+// console.log(x);
+
+// let const and var keywords. we don't use var keyword in today's gen.
+
+// let is used to define a varaiable ,that variable cannot be redeclared but can be updated
+// let age = 23;
+// age =34;
+// console.log(age); 
+//const is also used to define a variable here the variable cannot be redeclared or updated.
+// const name = " abinash ";
+//  here it won't  throw any error but after execution it shows error
+// name = ' abinash ';
+
+//Data types in js :-(primitive)
+// number
+// let age = 21;
+// console.log(age);
+// console.log(typeof(age));
+// let price = 1000.5;
+// console.log(typeof(price));
+// // string 
+// let name = "abinash ";
+// console.log(typeof(name));
+// // boolean
+// let isFollow = true;
+// console.log(typeof(isFollow));
+// // undefined
+// let a;
+// console.log(typeof(a));
+// // null
+// let x = null;
+// console.log(typeof(x));
+// // bigint 
+// let b = BigInt("2345");
+// console.log(typeof(b));
+// // symbol
+// let y = Symbol("hello!");
+// console.log(typeof(y));
+
+//objects(non-primitive)
+// const student = {
+//     name : "abinash",
+//     age : 20,
+//     cgpa : 9.2,
+//     isPass : true,
+// };
+// console.log(student);
+// console.log(typeof(student));
+// console.log(student["name"]);
+// console.log(student.age);
+// student.age = student.age + 2;
+// console.log(student.age);
+// student.name = "barsha";
+// console.log(student.name);
+
+// prctice set - 01 (Q!)
+// const product = {
+//     product_name : "Parker Jotter Standard CT Ball pen(Black)",
+//     rating : 4.3 ,
+//     isDeal : true ,
+//     price : 285 ,
+//     offer : 285*(5/100),
+//     price : 285 - 17.25 ,    
+// };
+// console.log(product);
+// console.log(product.isDeal);
+
+// Q2 
+// const profile = {
+//     username : "abinash304" ,
+//     isFollow : true,
+//     isMessage : false ,
+//     posts : 210 ,
+//     followers : 200000,
+//     following : 4 ,
+//     full_name : "Abinash Sahoo" ,
+//     thread : "@abinash304" ,
+//     status : "student" , 
+//     education_center :"GIET" ,
+// };
+// console.log(profile);
+// console.log(profile.posts);
