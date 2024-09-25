21c761a8636dcc2c18387b3113ac192855da40c4
Abinash Sahoo <abinashsahoo063@gmail.com>
2024-09-25 22:56:32 +0530

    comment COMPLETEJAVASCRIPT

---
+++ CHAP3JSANDHTML/forloop.js
@@ -0,0 +1,41 @@
+//LOOPING STATEMENTS IN JAVASCIPT
+
+//print some numbers using for loop in js 
+// let a = 5;
+// for(let i = 1;i <=a;i++){
+//     console.log(i)
+// }
+
+// PROGRAM TO ADD FIRST N NATURAL NUMBERS 
+// let sum = 0;
+// let num = prompt("enter a number");
+// num = Number.parseInt(num);
+// for(i =0;i<=num;i++){
+//     sum += i;
+// }
+// console.log('sum of n natural numbers is:',sum);
+
+//FACTORIAL OF A NUMBER USING FOR LOOP
+// let fact = 1;
+// let number=prompt('enter the number');
+// for(let i =1;i<=number;i++){
+//     fact *= i 
+// }
+// console.log("factorial is:",fact);
+
+//FOR-IN LOOP
+// let obj = {
+//     abinash:90,
+//     abhijit:34,
+//     deepak:89,
+//     mahesh:23
+// };
+// for(let a in obj){
+//     console.log("marks of " +a+ " is " +obj[a]);
+// }
+
+
+// FOR-OF LOOP
+for(let a of "abinash"){
+    console.log(a)
+}
